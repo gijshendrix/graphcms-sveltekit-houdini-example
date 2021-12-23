9078190e49562122366aaaa276b19f97ecf26f8b
Gijs Hendrix <gphendrix@gmail.com>
2021-12-23 21:23:06 +0100

    Styling fixes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -21,14 +21,14 @@
 <style>
 	main {
 		position: relative;
-		max-width: 56em;
+		max-width: 90em;
 		background-color: white;
 		padding: 2em;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
 	nav {
-		border-bottom: 1px solid rgba(255, 62, 0, 0.1);
+		border-bottom: 1px solid lightgray;
 		font-weight: 300;
 		padding: 0 1em;
 	}
